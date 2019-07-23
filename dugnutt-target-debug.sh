6ab986a1e206a5216bd998d22daae29a34350987
Barrett Brown <barrett.brown@geappliances.com>
2019-07-23 09:32:27 -0400

    Fix build scripts

--- dugnutt-target-debug.sh
+++ dugnutt-target-debug.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
+
 OPERATING_SYSTEM=`uname`
 
-if [ $OPERATING_SYSTEM == Linux ]; then
+if [[ $OPERATING_SYSTEM == 'Linux' ]]; then
   MAKE=make
-elif [ $OPERATING_SYSTEM == Darwin ]; then
+elif [[ $OPERATING_SYSTEM == 'Darwin' ]]; then
   MAKE=dmake
 else
-  echo "Unsupported operating system!"
+  echo 'Unsupported operating system!'
   exit 1
 fi
 
