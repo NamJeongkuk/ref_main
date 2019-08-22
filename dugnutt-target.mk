8b6fff953bd8cad2998a9d18dab4729cfa37536d
Ryan <ryan.hartlage@geappliances.com>
2019-08-22 10:18:16 -0400

    Hopefully fix skynet build

--- dugnutt-target.mk
+++ dugnutt-target.mk
@@ -117,6 +117,8 @@ $(call add_to_package,$(OUTPUT_DIR)/doc,doc)
 $(call add_to_package,$(OUTPUT_DIR)/$(TARGET).map,)
 $(call add_to_package,$(OUTPUT_DIR)/$(TARGET)_memory_usage_report.md,)
 
+include tools/kpit-rx/kpit-rx-makefile-worker.mk
+
 .PHONY: all
 all: target $(OUTPUT_DIR)/$(TARGET)_bootloader_app.mot
 	$(call copy_file,$(OUTPUT_DIR)/$(TARGET).apl,$(OUTPUT_DIR)/$(TARGET).mot)
@@ -155,5 +157,3 @@ upload: $(call upload_deps,all jlink_tools)
 .PHONY: clean
 clean: target_clean
 	$(MAKE) -C $(BOOT_LOADER_DIR) -f $(TARGET)-boot-loader.mk RELEASE=Y DEBUG=N clean
-
-include tools/kpit-rx/kpit-rx-makefile-worker.mk
