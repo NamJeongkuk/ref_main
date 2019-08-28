b37c61f690043f5546cf1576ab35a0ea4a137a90
Barrett Brown <barrett.brown@geappliances.com>
2019-08-28 09:31:50 -0400

    Update src/TinyApplication/DataSource/TinySystemErds.h
    Co-Authored-By: Ryan Hartlage <ryanplusplus@gmail.com>

--- src/TinyApplication/DataSource/TinySystemErds.h
+++ src/TinyApplication/DataSource/TinySystemErds.h
@@ -16,7 +16,7 @@
 #define ERD_TABLE(ENTRY) \
    ENTRY(Erd_ApplicationVersion,       0x003A, Version_t) \
    ENTRY(Erd_ButtonState,              0xF000, bool) \
-   ENTRY(Erd_LedState,                 0xF001, bool)
+   ENTRY(Erd_LedState,                 0xF001, bool) \
 
 #define EXPAND_AS_ENUM(Name, Number, DataType) \
    Name = Number COMMA
