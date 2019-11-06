800e13bc8e66a33b3cc014663e78af22756efc9a
Barrett Brown <barrettb@ieee.org>
2019-11-06 14:36:09 -0500

    Add boot loader to makefile

--- bonzalez-target.mk
+++ bonzalez-target.mk
@@ -1,11 +1,11 @@
 include tools/sdcc-stm8/sdcc-stm8.mk
 
 TARGET:=bonzalez
-
-OUTPUT_DIR:=build/bonzalez
-PROJECT_DIR=src
+OUTPUT_DIR:=build/$(TARGET)
 APPLCOMMON_TINY_DIR=lib/applcommon-tiny
 APPLCOMMON_DIR=$(APPLCOMMON_TINY_DIR)/lib/applcommon
+PROJECT_DIR=src
+BOOT_LOADER_DIR=lib/boot-loaders
 
 # Configures the STM8S headers for your part
 DEVICE_TYPE:=STM8S003
@@ -52,21 +52,36 @@ IGNORE_WARNINGS=24
 # OPTIMIZE_MAX_ALLOCS_PER_NODE:=50000
 
 PACKAGE_CONTENTS:=
-$(call add_to_package,$(OUTPUT_DIR)/$(TARGET).hex,)
+$(call add_to_package,$(OUTPUT_DIR)/binaries,binaries)
 $(call add_to_package,$(OUTPUT_DIR)/$(TARGET).map,)
 $(call add_to_package,$(OUTPUT_DIR)/$(TARGET)_memory_usage_report.md,)
 
 .PHONY: all
-all: $(OUTPUT_DIR)/$(TARGET).hex
+all: target $(OUTPUT_DIR)/$(TARGET)_bootloader_app.hex
+	$(call make_directory,$(OUTPUT_DIR)/binaries)
+	@$(LUA53) $(LUA_VERSION_RENAMER) --input $(OUTPUT_DIR)/$(TARGET).hex --endianness little --output_directory $(OUTPUT_DIR)/binaries
+	@$(LUA53) $(LUA_VERSION_RENAMER) --input $(OUTPUT_DIR)/$(TARGET)_bootloader_app.hex --endianness little --output_directory $(OUTPUT_DIR)/binaries --base_name $(TARGET).hex
+	@$(LUA53) $(LUA_VERSION_RENAMER) --input $(BOOT_LOADER_DIR)/build/$(TARGET)-boot-loader/$(TARGET)-boot-loader.hex --endianness little --output_directory $(OUTPUT_DIR)/binaries --base_name $(TARGET).hex
 	@$(LUA53) $(LUA_MEMORY_USAGE_REPORT) --configuration bonzalez_memory_report_config.lua --output $(OUTPUT_DIR)/$(TARGET)_memory_usage_report.md
 	@echo Build complete
 
+.PHONY: target
+target: $(OUTPUT_DIR)/$(TARGET).htiny $(OUTPUT_DIR)/$(TARGET).tiny
+
+.PHONY: boot-loader
+boot-loader:
+	$(MAKE) -C $(BOOT_LOADER_DIR) -f $(TARGET)-boot-loader.mk RELEASE=Y DEBUG=N
+
+$(OUTPUT_DIR)/$(TARGET)_bootloader_app.hex: target boot-loader
+	@$(LUA53) $(LUA_INTEL_HEX_CONCATENATE) --input $(BOOT_LOADER_DIR)/build/$(TARGET)-boot-loader/$(TARGET)-boot-loader.hex $(OUTPUT_DIR)/$(TARGET).no_vectors.htiny --output $@
+
 .PHONY: upload
-upload: $(OUTPUT_DIR)/$(TARGET).hex stm8flash
+upload: $(OUTPUT_DIR)/$(TARGET)_bootloader_app.hex stm8flash
 	$(call stm8flash_upload,$(STM8FLASH_PROGRAMMER),$(STM8FLASH_PART),$^)
 
 .PHONY: clean
 clean: target_clean
+	$(MAKE) -C $(BOOT_LOADER_DIR) -f $(TARGET)-boot-loader.mk RELEASE=Y DEBUG=N clean
 
 .PHONY: package
 package: all
