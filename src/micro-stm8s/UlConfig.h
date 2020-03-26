fba0d64651a38094ecff483232d87e73e99ee442
Ryan <ryan.hartlage@geappliances.com>
2020-03-26 16:52:34 -0400

    Use enum instead of defines for UL config

--- src/micro-stm8s/UlConfig.h
+++ src/micro-stm8s/UlConfig.h
@@ -11,27 +11,30 @@
 #include "MemoryMap.h"
 #include "Crc16.h"
 
-// The address of the expected CRC
-// This should be the second-to-last byte of the application
-// image if using no boot loader or the STM8-specific boot
-// loader
-// This should be the address of the application image header
-// if using a full boot loader
-#define RomCheckCrcAddress ApplicationStart
+enum
+{
+   // The address of the expected CRC
+   // This should be the second-to-last byte of the application
+   // image if using no boot loader or the STM8-specific boot
+   // loader
+   // This should be the address of the application image header
+   // if using a full boot loader
+   RomCheckCrcAddress = ApplicationStart,
 
-// Where the ROM check should begin
-// This should be the first byte of the application image
-// if using no boot loader or the STM8-specific boot loader
-// This should be the address immediately following the
-// image CRC in the application header if using a full boot
-// loader
-#define RomCheckStart (ApplicationStart + sizeof(Crc16_t))
+   // Where the ROM check should begin
+   // This should be the first byte of the application image
+   // if using no boot loader or the STM8-specific boot loader
+   // This should be the address immediately following the
+   // image CRC in the application header if using a full boot
+   // loader
+   RomCheckStart = (ApplicationStart + sizeof(Crc16_t)),
 
-// The last byte of ROM that will be checked
-#define RomCheckStop ApplicationEnd
+   // The last byte of ROM that will be checked
+   RomCheckStop = ApplicationEnd,
 
-// How many bytes should be checked each time the periodic
-// ROM test is run
-#define RomCheckStep 10
+   // How many bytes should be checked each time the periodic
+   // ROM test is run
+   RomCheckStep = 10
+};
 
 #endif
--- src/nano-stm8s/UlConfig.h
+++ src/nano-stm8s/UlConfig.h
@@ -8,27 +8,30 @@
 #ifndef ULCONFIG_H
 #define ULCONFIG_H
 
-// The address of the expected CRC
-// This should be the second-to-last byte of the application
-// image if using no boot loader or the STM8-specific boot
-// loader
-// This should be the address of the application image header
-// if using a full boot loader
-#define RomCheckCrcAddress 0x87FE
+enum
+{
+   // The address of the expected CRC
+   // This should be the second-to-last byte of the application
+   // image if using no boot loader or the STM8-specific boot
+   // loader
+   // This should be the address of the application image header
+   // if using a full boot loader
+   RomCheckCrcAddress = 0x87FE,
 
-// Where the ROM check should begin
-// This should be the first byte of the application image
-// if using no boot loader or the STM8-specific boot loader
-// This should be the address immediately following the
-// image CRC in the application header if using a full boot
-// loader
-#define RomCheckStart 0x8440
+   // Where the ROM check should begin
+   // This should be the first byte of the application image
+   // if using no boot loader or the STM8-specific boot loader
+   // This should be the address immediately following the
+   // image CRC in the application header if using a full boot
+   // loader
+   RomCheckStart = 0x8440,
 
-// The last byte of ROM that will be checked
-#define RomCheckStop 0x87FD
+   // The last byte of ROM that will be checked
+   RomCheckStop = 0x87FD,
 
-// How many bytes should be checked each time the periodic
-// ROM test is run
-#define RomCheckStep 1
+   // How many bytes should be checked each time the periodic
+   // ROM test is run
+   RomCheckStep = 10
+};
 
 #endif
