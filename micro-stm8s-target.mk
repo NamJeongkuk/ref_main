088003c24e2167802cb985dc56c306f29bd883f9
Ryan <ryan.hartlage@geappliances.com>
2020-04-01 17:13:24 -0400

    Fixed unsightly EWS

--- micro-stm8s-target.mk
+++ micro-stm8s-target.mk
@@ -60,7 +60,6 @@ COMMON_LIB_DIRS:=\
    $(APPLCOMMON_TINY_DIR)/src/Hardware/Stm8/Ul \
    $(APPLCOMMON_TINY_DIR)/src/TinyLib \
 
-
 INC_DIRS:=\
    $(APPLCOMMON_DIR)/Utilities \
    $(APPLCOMMON_TINY_DIR)/src/BootLoader \
