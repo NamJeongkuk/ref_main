4a6bfe2d30117574fb7f5e9f2f89da72dd8cb03b
Ryan <ryan.hartlage@geappliances.com>
2020-04-14 16:26:48 -0400

    Work around sdcc bug... again

--- src/micro-stm8s/Plugins/ApplianceApiPlugin.c
+++ src/micro-stm8s/Plugins/ApplianceApiPlugin.c
@@ -12,7 +12,8 @@
 #include "MemoryMap.h"
 #include "utils.h"
 
-enum {
+enum
+{
    ModelParametricItem = 1,
    SerialParametricItem = 2,
    Personality = 0
@@ -34,7 +35,12 @@ static void DataSourceChanged(void *context, const void *_args)
    {
       TinyTimerModule_t *timerModule;
       TinyDataSource_Read(instance->_private.dataSource, Erd_TimerModule, &timerModule);
-      TinyTimerModule_Start(timerModule, &instance->_private.resetTimer, *((uint8_t *)args->data), ResetTimerExpired, NULL);
+      TinyTimerModule_Start(
+         timerModule,
+         &instance->_private.resetTimer,
+         *((uint8_t *)args->data) * (TinyTimerTicks_t)1000,
+         ResetTimerExpired,
+         NULL);
    }
 }
 
@@ -48,34 +54,34 @@ void ApplianceApiPlugin_Init(
    uint8_t length;
    ModelNumber_t modelSerial;
    TinyBootLoaderParametric_ReadItem(BootLoaderHeaderAddress, ModelParametricItem, &modelSerial, &length);
-   TinyDataSource_Write(dataSource, Erd_ModelNumber, &modelSerial);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_ModelNumber, &modelSerial);
    TinyBootLoaderParametric_ReadItem(BootLoaderHeaderAddress, SerialParametricItem, &modelSerial, &length);
-   TinyDataSource_Write(dataSource, Erd_SerialNumber, &modelSerial);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_SerialNumber, &modelSerial);
 #endif
 
    ApplianceType_t applianceType = ApplianceType_Dishwasher;
-   TinyDataSource_Write(dataSource, Erd_ApplianceType, &applianceType);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_ApplianceType, &applianceType);
 
    bool readyToEnterBootLoader = true;
-   TinyDataSource_Write(dataSource, Erd_ReadyToEnterBootLoader, &readyToEnterBootLoader);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_ReadyToEnterBootLoader, &readyToEnterBootLoader);
 
    AppliancePersonality_t personality = 0;
-   TinyDataSource_Write(dataSource, Erd_Personality, &personality);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_Personality, &personality);
 
    uint8_t supportedImageTypes = (1 << ImageType_BootLoader) | (1 << ImageType_Application);
-   TinyDataSource_Write(dataSource, Erd_SupportedImageTypes, &supportedImageTypes);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_SupportedImageTypes, &supportedImageTypes);
 
    const ImageHeader_t *header = BootLoaderHeaderAddress;
-   TinyDataSource_Write(dataSource, Erd_BootLoaderVersion, &header->criticalMajorVersion);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_BootLoaderVersion, &header->criticalMajorVersion);
    header = ApplicationHeaderAddress;
-   TinyDataSource_Write(dataSource, Erd_ApplicationVersion, &header->criticalMajorVersion);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_ApplicationVersion, &header->criticalMajorVersion);
 
    ApplianceApiManifest_t applianceApiManifest = {
       1,
       ApplianceApi_Version1_Primary | ApplianceApi_Version1_BootLoader
    };
-   TinyDataSource_Write(dataSource, Erd_ApplianceApiManifest, &applianceApiManifest);
+   TinyDataSource_Write(instance->_private.dataSource, Erd_ApplianceApiManifest, &applianceApiManifest);
 
    TinyEventSubscription_Init(&instance->_private.dataChangedSubscription, instance, DataSourceChanged);
-   TinyEvent_Subscribe(TinyDataSource_GetOnChangeEvent(dataSource), &instance->_private.dataChangedSubscription);
+   TinyEvent_Subscribe(TinyDataSource_GetOnChangeEvent(instance->_private.dataSource), &instance->_private.dataChangedSubscription);
 }
