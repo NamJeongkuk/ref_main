b6e46fa5fdd9b5eece123ff900d8eb0931d8130d
Ryan <ryan.hartlage@geappliances.com>
2020-04-20 17:09:53 -0400

    ROM optimizations for micro-stm8s

--- src/MicroApplication/DataSource/MicroSystemData.c
+++ src/MicroApplication/DataSource/MicroSystemData.c
@@ -6,6 +6,21 @@
  */
 
 #include "MicroSystemData.h"
+#include "utils.h"
+
+#define EXPAND_AS_OFFSET_STRUCT_MEMBER(Name, Number, DataType, Stream, RemoteErd) \
+   uint8_t MACRO_SAFE_CONCATENATE(erd, Name)[sizeof(DataType)];
+
+typedef struct
+{
+   ERD_TABLE(EXPAND_AS_OFFSET_STRUCT_MEMBER)
+} TinyRamDataSourceStorage_t;
+
+static struct
+{
+   TinyDataSource_Ram_t tinyRamDataSource;
+   TinyRamDataSourceStorage_t erdRam;
+} instance;
 
 #define EXPAND_AS_DATASOURCE_TINY_RAM_ERD_ENTRY(Name, Number, DataType, Stream, RemoteErd) \
    { Name, sizeof(DataType) },
@@ -19,20 +34,20 @@ static const TinyDataSource_RamConfiguration_t configuration = {
    NUM_ELEMENTS(erdTable)
 };
 
-void MicroSystemData_Init(MicroSystemData_t *instance, TinyTimerModule_t *timerModule)
+void MicroSystemData_Init(TinyTimerModule_t *timerModule)
 {
    TinyDataSource_Ram_Init(
-      &instance->_private.tinyRamDataSource,
+      &instance.tinyRamDataSource,
       &configuration,
-      &instance->_private.erdRam);
+      &instance.erdRam);
 
    TinyDataSource_Write(
-      &instance->_private.tinyRamDataSource.interface,
+      &instance.tinyRamDataSource.interface,
       Erd_TimerModule,
       &timerModule);
 }
 
-I_TinyDataSource_t *MicroSystemData_DataSource(MicroSystemData_t *instance)
+I_TinyDataSource_t *MicroSystemData_DataSource(void)
 {
-   return &instance->_private.tinyRamDataSource.interface;
+   return &instance.tinyRamDataSource.interface;
 }
--- src/MicroApplication/DataSource/MicroSystemData.h
+++ src/MicroApplication/DataSource/MicroSystemData.h
@@ -11,35 +11,15 @@
 #include "TinyDataSource_Ram.h"
 #include "TinyTimer.h"
 #include "MicroSystemErds.h"
-#include "utils.h"
-
-#define EXPAND_AS_OFFSET_STRUCT_MEMBER(Name, Number, DataType, Stream, RemoteErd) \
-   uint8_t MACRO_SAFE_CONCATENATE(erd, Name)[sizeof(DataType)];
-
-typedef struct
-{
-   ERD_TABLE(EXPAND_AS_OFFSET_STRUCT_MEMBER)
-} TinyRamDataSourceStorage_t;
-
-typedef struct
-{
-   struct
-   {
-      TinyDataSource_Ram_t tinyRamDataSource;
-      TinyRamDataSourceStorage_t erdRam;
-   } _private;
-} MicroSystemData_t;
 
 /*!
- * @param instance
  * @param timerModule
  */
-void MicroSystemData_Init(MicroSystemData_t *instance, TinyTimerModule_t *timerModule);
+void MicroSystemData_Init(TinyTimerModule_t *timerModule);
 
 /*!
- * @param instance
  * @return
  */
-I_TinyDataSource_t *MicroSystemData_DataSource(MicroSystemData_t *instance);
+I_TinyDataSource_t *MicroSystemData_DataSource(void);
 
 #endif
