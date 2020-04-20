2a4cf20f51c571fc45984a6fa20dc96a6f82027e
Ryan <ryan.hartlage@geappliances.com>
2020-04-20 15:38:16 -0400

    Ran formatter on PSoC shim thing

--- src/micro-psoc4100s-cap-touch/PsocShim/CapTouch_ISR.h
+++ src/micro-psoc4100s-cap-touch/PsocShim/CapTouch_ISR.h
@@ -24,21 +24,21 @@ void CapTouch_ISR_ClearPending(void);
 /* Interrupt Controller Constants */
 
 /* Address of the CapTouch_ISR ISR priority. */
-#define CapTouchIsrInterruptPriority             ((reg32 *) CapTouch_ISR__INTC_PRIOR_REG)
+#define CapTouchIsrInterruptPriority ((reg32 *)CapTouch_ISR__INTC_PRIOR_REG)
 
 /* Priority of the CapTouch_ISR interrupt. */
-#define CapTouchIsrInterruptPriorityNumber      CapTouch_ISR__INTC_PRIOR_NUM
+#define CapTouchIsrInterruptPriorityNumber CapTouch_ISR__INTC_PRIOR_NUM
 
 /* Address of the INTC.SET_EN[x] byte to bit enable CapTouch_ISR interrupt. */
-#define CapTouchIsrInterruptSetEnable            ((reg32 *) CapTouch_ISR__INTC_SET_EN_REG)
+#define CapTouchIsrInterruptSetEnable ((reg32 *)CapTouch_ISR__INTC_SET_EN_REG)
 
 /* Address of the INTC.CLR_EN[x] register to bit clear the CapTouch_ISR interrupt. */
-#define CapTouchIsrInterruptClearEnable            ((reg32 *) CapTouch_ISR__INTC_CLR_EN_REG)
+#define CapTouchIsrInterruptClearEnable ((reg32 *)CapTouch_ISR__INTC_CLR_EN_REG)
 
 /* Address of the INTC.SET_PD[x] register to set the CapTouch_ISR interrupt state to pending. */
-#define CapTouchIsrInterruptSetPending            ((reg32 *) CapTouch_ISR__INTC_SET_PD_REG)
+#define CapTouchIsrInterruptSetPending ((reg32 *)CapTouch_ISR__INTC_SET_PD_REG)
 
 /* Address of the INTC.CLR_PD[x] register to clear the CapTouch_ISR interrupt. */
-#define CapTouchIsrInterruptClearPending            ((reg32 *) CapTouch_ISR__INTC_CLR_PD_REG)
+#define CapTouchIsrInterruptClearPending ((reg32 *)CapTouch_ISR__INTC_CLR_PD_REG)
 
 #endif
