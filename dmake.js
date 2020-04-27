1e2e3766b36567884a651554472e5fcbaf083969
Chris Beeler <cbeeler@users.noreply.github.com>
2020-04-27 13:27:07 -0400

    Update dmake.js

--- dmake.js
+++ dmake.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const kpit_rx_base = require('./tools/kpit-rx/dmake');
-const iar_stm8_base = require('./tools/iar-stm8-wine/dmake');
 
 module.exports = {
   'rx130-target.mk': {
@@ -25,28 +24,6 @@ module.exports = {
         'artifacts',
         'lib/boot-loaders/build',
         'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'stm8s-target.mk': {
-    ...iar_stm8_base,
-    options: {
-      ...iar_stm8_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build'
-      ]
-    }
-  },
-  'stm8s-cap-touch-target.mk': {
-    ...iar_stm8_base,
-    options: {
-      ...iar_stm8_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build'
       ]
     }
   },
