acf02b3b920090118d74b742869ab09c735ce084
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-11 15:57:46 -0500

    try just reading from where the meta data gets stored

--- src/rx130/main.c
+++ src/rx130/main.c
@@ -188,13 +188,13 @@ int main(void)
    HardwareEeprom_I2c_t *hardwareEepromI2c;
    hardwareEepromI2c = HardwareEeprom_I2c_Init(watchdogKickAction);
    // uint8_t writeBuffer[] = {0xA5};
-   uint8_t writeBuffer[] = { 0xA5, 0xB5 };
-   // hardwareeeprom_write(..., writeBuffer, sizeof(writeBuffer))
-   HardwareEeprom_Write(&hardwareEepromI2c->interface, 12, 2, writeBuffer);
+   // uint8_t writeBuffer[] = { 0xA5, 0xB5, 0xC3, 0xB8 };
+   // // hardwareeeprom_write(..., writeBuffer, sizeof(writeBuffer))
+   // HardwareEeprom_Write(&hardwareEepromI2c->interface, 12, 4, writeBuffer);
    // uint8_t readBuffer[sizeof(writeBuffer)];
    uint8_t readBuffer[sizeof(writeBuffer)];
    // hardwareeeprom_read(..., readBuffer, sizeof(readBuffer))
-   HardwareEeprom_Read(&hardwareEepromI2c->interface, 12, 2, readBuffer);
+   HardwareEeprom_Read(&hardwareEepromI2c->interface, 12, 4, readBuffer);
    // check whether what you wrote is what you read back
    if(readBuffer[0] == writeBuffer[0])
    {
