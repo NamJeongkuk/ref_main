531455921266046401624569b72e1d802b572bae
Grant <gl1miller@yahoo.com>
2022-01-12 13:06:52 -0500

    remove unused src files from other boards

--- dmake.js
+++ dmake.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const kpit_rx_base = require('./tools/kpit-rx/dmake');
-const kpit_rl78_base = require('./tools/kpit-rl78/dmake');
-const llvm_rl78_base = require('./tools/llvm-rl78/dmake');
 
 module.exports = {
   'rx130-target.mk': {
@@ -14,110 +12,6 @@ module.exports = {
         'artifacts',
         'lib/boot-loaders/build',
         'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'rx231-target.mk': {
-    ...kpit_rx_base,
-    options: {
-      ...kpit_rx_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build',
-        'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'mini-rx130-target.mk': {
-    ...kpit_rx_base,
-    options: {
-      ...kpit_rx_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build',
-        'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'mini-rx130-cap-touch-target.mk': {
-    ...kpit_rx_base,
-    options: {
-      ...kpit_rx_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build',
-        'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'mini-rx231-target.mk': {
-    ...kpit_rx_base,
-    options: {
-      ...kpit_rx_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build',
-        'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'micro-rl78g13-target.mk': {
-    ...kpit_rl78_base,
-    options: {
-      ...kpit_rl78_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build',
-        'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'micro-rx130-target.mk': {
-    ...kpit_rx_base,
-    options: {
-      ...kpit_rx_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build',
-        'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'micro-rx130-cap-touch-target.mk': {
-    ...kpit_rx_base,
-    options: {
-      ...kpit_rx_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts',
-        'lib/boot-loaders/build',
-        'lib/boot-loaders/lib/boot-loader-updater/build'
-      ]
-    }
-  },
-  'nano-rl78g12-target.mk': {
-    ...llvm_rl78_base,
-    options: {
-      ...llvm_rl78_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts'
-      ]
-    }
-  },
-  'nano-rl78g13-target.mk': {
-    ...llvm_rl78_base,
-    options: {
-      ...llvm_rl78_base.options,
-      outputDirectories: [
-        'build',
-        'artifacts'
       ]
     }
   }
