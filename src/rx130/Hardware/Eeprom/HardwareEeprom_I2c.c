82a489b43646fc2ee24ab474a48aee0af332b3cd
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-13 11:25:38 -0500

    use old toolchain for now; need to clean out all my error stuff

--- src/rx130/Hardware/Eeprom/HardwareEeprom_I2c.c
+++ src/rx130/Hardware/Eeprom/HardwareEeprom_I2c.c
@@ -222,6 +222,7 @@ static HardwareEepromErrorSource_t Read_Eeprom_Hardware(
    /* Declare a dummy variable */
    volatile uint8_t dummyRead = 0;
    volatile uint32_t u32WaitCount = EepromMaxWaitingCount;
+   volatile uint32_t specialWaitCount = 4 * EepromMaxWaitingCount;
 
    /* Reset the data received counter */
    instance._private.receiveDataCount = 0;
@@ -320,7 +321,7 @@ static HardwareEepromErrorSource_t Read_Eeprom_Hardware(
    /* Wait until all expected data has been
     received or until a NACKF has been detected */
    while(!AllBytesHaveBeenRead() && (RIIC0.ICSR2.BIT.NACKF == 0))
-      DECREASE_COUNT_EXIT_IF_ZERO(u32WaitCount, HardwareEepromErrorSource_12);
+      DECREASE_COUNT_EXIT_IF_ZERO(specialWaitCount, HardwareEepromErrorSource_12);
 
    /* Reception was terminated due to NACK reception */
    if(1 == RIIC0.ICSR2.BIT.NACKF)
