b8adacd37fba2b089127ffb682276b2866923ae6
Kira Hartlage <kira.hartlage@gmail.com>
2022-01-19 10:57:49 -0500

    add refer data model test double

---
+++ test/Mocks/ReferDataModel_TestDouble.h
@@ -0,0 +1,46 @@
+/*!
+ * @file
+ * @brief
+ *
+ * Copyright GE Appliances - Confidential - All rights reserved.
+ */
+
+#ifndef REFERDATAMODEL_TESTDOUBLE_H
+#define REFERDATAMODEL_TESTDOUBLE_H
+
+#include "ReferDataModel_TestDouble.h"
+#include "I_DataModel.h"
+#include "Timer.h"
+#include "SystemData.h"
+#include "TimerModule_TestDouble.h"
+#include "Action_Context.h"
+#include "Eeprom_Model.h"
+
+typedef struct
+{
+   I_DataModel_t *dataModel;
+   I_DataSource_t *externalDataSource;
+
+   struct
+   {
+      SystemData_t systemData;
+      TimerModule_TestDouble_t timerModuleTestDouble;
+      Action_Context_t runTimerModuleAction;
+      Eeprom_Model_t eepromModel;
+   } _private;
+} ReferDataModel_TestDouble_t;
+
+/*!
+ * @brief Refer DataModel TestDouble
+ * @param instance
+ */
+void ReferDataModel_TestDouble_Init(ReferDataModel_TestDouble_t *instance);
+
+/*!
+ * @brief TimerModule TestDouble
+ * @param instance
+ * @return pointer to the TimerModule TestDouble
+ */
+TimerModule_TestDouble_t *ReferDataModel_TestDouble_GetTimerModuleTestDouble(ReferDataModel_TestDouble_t *instance);
+
+#endif
