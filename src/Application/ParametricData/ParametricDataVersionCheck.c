f81e1d491d1bcc26eccb0fb09348f1123a92c5a2
Leonardo Falcon <Leonardo.FalconIzzi@geappliances.com>
2023-06-15 09:52:12 -0400

    Bump versions for release

--- src/Application/ParametricData/ParametricDataVersionCheck.c
+++ src/Application/ParametricData/ParametricDataVersionCheck.c
@@ -12,7 +12,7 @@
 enum
 {
    Critical_MajorVersion = 0,
-   Critical_MinorVersion = 10
+   Critical_MinorVersion = 11
 };
 
 void ParametricDataVersionCheck_Init(I_Action_t *jumpToBootloaderAction)
