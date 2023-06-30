d20cc2283db365cc15b5700e26ed96202438384d
Leonardo Falcon Izzi <leonardo.falconizzi@geappliances.com>
2023-06-30 08:49:49 -0400

    #1611 Add fresh food thermistor lookup table (#1618)
    * Add fresh food thermistor lookup table
    
    * Fix

---
+++ Parametric/data/Development/Sensor/fresh_food_temperature_sensor_qa.lua
@@ -0,0 +1,24 @@
+-- FF Cabinet (LNTD6351G (FF)), divider: 4.75K, precise zone between [2800, 6000], precise zone max error: 5, outside zone max error: 25 degf_x100
+return {
+  data = {
+    { raw = 6144, mapped = 13996 },
+    { raw = 7360, mapped = 12994 },
+    { raw = 8832, mapped = 11984 },
+    { raw = 10752, mapped = 10906 },
+    { raw = 13056, mapped = 9839 },
+    { raw = 15936, mapped = 8729 },
+    { raw = 19584, mapped = 7550 },
+    { raw = 24320, mapped = 6246 },
+    { raw = 26880, mapped = 5609 },
+    { raw = 30272, mapped = 4811 },
+    { raw = 34816, mapped = 3791 },
+    { raw = 41664, mapped = 2270 },
+    { raw = 48256, mapped = 695 },
+    { raw = 52608, mapped = -511 },
+    { raw = 55808, mapped = -1571 },
+    { raw = 58176, mapped = -2529 },
+    { raw = 60032, mapped = -3463 },
+    { raw = 60864, mapped = -3968 }
+  },
+  invalid_value = 30000
+}
