4e8bfced637a8b55f5890ef97f037d7d17d487ac
Trevor Aten <traten11@gmail.com>
2023-09-19 16:28:54 -0400

    [No Ticket] Rev Parametric Version Check Number to Match Parametric (#1871)
    * rev parametric data version check
    
    * tests

--- src/Application/ParametricData/ParametricDataVersionCheck.c
+++ src/Application/ParametricData/ParametricDataVersionCheck.c
@@ -12,7 +12,7 @@
 enum
 {
    Critical_MajorVersion = 0,
-   Critical_MinorVersion = 15
+   Critical_MinorVersion = 16
 };
 
 void ParametricDataVersionCheck_Init(I_Action_t *jumpToBootloaderAction)
