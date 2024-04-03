bd01cdb6f25545f194fe94e7b3a65240f1186d4e
Leonardo Falcon Izzi <leonardo.falconizzi@geappliances.com>
2024-04-03 16:11:35 -0400

    [No ticket] New Parametric Summary (#2517)
    * New Summary
    
    * Fix path
    
    * Add ingest
    
    * Fix path

---
+++ Parametric/ingest.lua
@@ -0,0 +1,72 @@
+--[[
+Generates a tree data structure representing the calls made in the parametric data and their arguments.
+This tree can be traversed to provide a summary of the parametric data.
+]]
+
+return function(path)
+   local global = {}
+ 
+   local function tostringify(t)
+     if (getmetatable(t) or {}).__tostring then
+       return tostring(t)
+     end
+ 
+     if type(t) == 'table' then
+       local newTable = {}
+       for k, v in pairs(t) do
+         local newIndex = type(k) == 'number' and k - 1 or k
+         newTable[newIndex] = tostringify(v)
+       end
+       return newTable
+     end
+ 
+     return t
+   end
+ 
+   local function MetaMetatable(base)
+     return {
+       __call = function(_, v)
+         local o = {}
+         o[base] = v
+ 
+         return o
+       end,
+ 
+       __add = function(a, b)
+         return tostring(a) .. ' + ' .. tostring(b)
+       end,
+ 
+       __bor = function(a, b)
+         return tostring(a) .. ' | ' .. tostring(b)
+       end,
+ 
+       __tostring = function()
+         return base
+       end,
+ 
+       __index = function(_, name)
+         return setmetatable({}, MetaMetatable(base and (base .. '.' .. name) or name))
+       end
+     }
+   end
+ 
+   local import = require 'lua-common'.util.EvalInCurrentEnvironment('./')
+   global.import = import
+   global.constants = require 'src/constants'
+   global.math = require 'lua-common'.math
+ 
+   setmetatable(_G, {
+     __index = function(_, name)
+       if global[name] then
+         return global[name]
+       end
+ 
+       return setmetatable({}, MetaMetatable(name))
+     end
+   })
+ 
+   local ingested = tostringify(import(path))
+   setmetatable(_G, nil)
+   return ingested
+ end
+ 
--- Parametric/summarize_parametric.lua
+++ Parametric/summarize_parametric.lua
@@ -5,18 +5,51 @@ end
 
 package.path = 'lib/lua-common/?.lua;' .. package.path
 
+local pairs_sorted_by_keys = require 'lua-common'.util.pairs_sorted_by_keys
 local printed = {}
 
 local function printer(s)
   table.insert(printed, s)
 end
 
-local pretty_print = require 'lua-common'.util.PrettyPrint(printer)
+local function string_ends_with(s, substring)
+  return (string.sub(s, 0 - #substring) == substring)
+end
+
+local function PrettyTable(print)
+  local function pretty_print(t, header)
+    if not header then header = "" end
+    local subHeader = header
+
+    if type(t) == 'table' then
+      for k, v in pairs_sorted_by_keys(t) do
+        local formatting = header;
+        local subHeaderIsPresent = string_ends_with(header, tostring(k) .. ".")
+
+        if not subHeaderIsPresent then
+          formatting = formatting .. k
+        end
+
+        if type(v) == 'table' then
+          subHeader = subHeaderIsPresent and formatting or formatting .. '.'
+          pretty_print(v, subHeader)
+        else
+          print(formatting .. ": " .. tostring(v))
+        end
+      end
+    else
+      print(tostring(t))
+    end
+  end
+
+  return pretty_print
+end
+
 local write_to_file = require 'lua-common'.io.write_to_file
-local ingest = require 'src.ingest'
+local pretty_print = PrettyTable(printer)
+local ingest = require 'ingest'
 
 local ingested = ingest(arg[1])
-
 pretty_print(ingested)
 
 write_to_file(arg[2], table.concat(printed, '\n'))
